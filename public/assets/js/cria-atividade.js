3b4e71c4b03cef44bba032601188aed7cc9da006
israelAbreu <israeloliveira18@hotmail.com>
2024-03-02 17:59:25 -0300

    feat: organização do projeto | validação de campos vázios

--- public/assets/js/cria-atividade.js
+++ public/assets/js/cria-atividade.js
@@ -15,18 +15,26 @@ function criarAtividade () {
     let descricao = document.getElementById('descricao').value
     let horario = document.getElementById('horario').value
 
-    atividade.id = Math.round(Math.random() * (100 - 1) + 1)
-    atividade.descricao = descricao
-    atividade.horario = horario
+    if (descricao.length < 1 || descricao == undefined ) {
+       window.alert("Você precisa preencher a descrição da atividade");
+    }
+    if (horario.length < 1 || horario === undefined) {
+      window.alert("Você precisa preencher o horário da atividade");
+    }
 
-    salvarAtividade()
-    manipularDom(atividade)
+    if (descricao != null && descricao.length > 4 && horario != null && horario.length > 5) {
+      atividade.id = Math.round(Math.random() * (100 - 1) + 1);
+      atividade.descricao = descricao;
+      atividade.horario = horario;
+
+      salvarAtividade();
+      manipularDom(atividade);
+    }
 
 }
 
 function getAtividades () {
     let listaAtividades = Object.values(localStorage);
-    console.log(listaAtividades);
 
     listaAtividades.forEach((element) => {
       let atividade = JSON.parse(element);
